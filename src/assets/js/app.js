198d4a7530ca0f6609978ba64db8cb8e4c35867a
Julien Lemonnier <contact@labfordev.fr>
2022-04-20 20:30:58 +0200

    add stimulus controller

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,3 +1,13 @@
+import { Application } from "stimulus";
+
+// Import SCSS
 import './../scss/app.scss';
 
-console.log('Hello Parcel!');
+// Import Stimulus controllers
+import HelloParcelController from './controllers/hello-parcel_controller';
+
+// Start Stimulus
+const application = Application.start();
+
+// Declaration Stimulus controllers
+application.register('hello-parcel', HelloParcelController);
